6c11f123c160bc567a242b569558b94a14a6da28
mhorowitzgelb <mhorowitzgelb@hotmail.com>
2015-02-13 00:35:30 -0600

    Did more on multihexagonal work

--- VoxelTutorial/Assets/GenerateHexagon.cs
+++ VoxelTutorial/Assets/GenerateHexagon.cs
@@ -10,13 +10,38 @@ public class GenerateHexagon : MonoBehaviour {
     List<int> triangles;
     List<Vector2> uvs;
     int vertexOffset = 0;
-
+    private const int WorldDiameter = 7;
     float Root3 = Mathf.Sqrt(3);
+    byte[][] data;
     // Use this for initialization
 	void Start () {
+        data = new byte[2*(WorldDiameter/2) + 1][];
+        for (int i = 0; i < WorldDiameter / 2; i++)
+        {
+            data[i] = new byte[WorldDiameter / 2 + i];    
+        }
+        for (int i = WorldDiameter / 2; i < WorldDiameter; i ++ )
+        {
+            data[i] = new byte[WorldDiameter - i + WorldDiameter / 2];
+        }
         vertices = new List<Vector3>();
+
+
         triangles = new List<int>();
         uvs = new List<Vector2>();
+
+        for (int q = -WorldDiameter / 2; q <= WorldDiameter/2; q++)
+        {
+            for (int r = -(WorldDiameter/2 - Mathf.Abs(q)); r <= (WorldDiameter/2 - Mathf.Abs(q)); r++)
+            {
+                Vector3 position = Vector3.zero;
+                position -= r * new Vector3(0, 0, Root3);
+                position -= q * new Vector3(1.5f, 0, Root3 / 2);
+                HexTop(position, 1);
+                SetBlock(q, r, 1);
+            }
+        }
+        /*
         Vector3 position = new Vector3(1, 1, 1);
         HexTop(position, 0);
         HexSideTopLeft(position, 0);
@@ -24,13 +49,24 @@ public class GenerateHexagon : MonoBehaviour {
         HexSideTopRight(position, 0);
         HexSideBottom(position, 0);
         HexSideBottomLeft(position, 0);
-        HexSideBottomRIght(position, 0);
+        HexSideBottomRIght(position, 0);*/
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.uv = uvs.ToArray();
 	}
+
+    byte GetBlock(int q, int r)
+    {
+        //
+        return data[q + WorldDiameter/2][r + WorldDiameter/2 + Mathf.Min(0, q)];
+    }
+
+    void SetBlock(int q, int r, byte b)
+    {
+        //data[q + WorldDiameter / 2][r + WorldDiameter / 2 + Mathf.Min(0, q)] = b;
+    }
 
     void HexTop(Vector3 topLeft, byte block){
        
