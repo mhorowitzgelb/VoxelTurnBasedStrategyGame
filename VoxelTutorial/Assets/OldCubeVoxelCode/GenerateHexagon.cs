bfc42c0aa0e1db39f41bb9943c2a466edcfc3a57
mhorowitzgelb <mhorowitzgelb@hotmail.com>
2015-04-19 22:33:11 -0500

    Reorginzed files and commented. Something got goofed in the selected tiles.

---
+++ VoxelTutorial/Assets/OldCubeVoxelCode/GenerateHexagon.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateHexagon : MonoBehaviour {
+
+    public const float TILE_HEIGHT = 0.5f;
+
+	public  const int chunkSize = 15;
+    public  const int WorldDiameter = 15*11;
+	public  const int WorldRadius = WorldDiameter / 2;
+    float Root3 = Mathf.Sqrt(3);
+    //List of utilized chunks in array position based off their actual chunkPosition
+    public HexChunk[,] chunks;
+
+    //The heightmap data position by position
+    byte[,,,] data;
+    public Dictionary<Vector2, HexPiece> pieces;
+    public GameObject piecePrefab;
+    public Dictionary<Vector2, int> selectedTiles;
+    //Free chunks that can be used to draw/update the map
+    //The key point of this is to prevent unity for instantiating and destroying a lot
+    //of objects and eating up memory. (Although it doesn't actually eat up the memory. The amount of allocated memory is fine
+    //but unity is stupid and decides to keep increasing the size of the heap when it doesn't actually need to do that.
+    //Thanks unity developers.
+    public List<HexChunk> freeChunks; 
+
+    //The chunk prefab we will be instantiating for drawing each chunk
+    public GameObject ChunkPrefab;
+
+    //The player/camera
+    public GameObject player;
+
+    //How far away when we start unloading chunks
+    public const int distToUnload = 180;
+
+    //How close when we start loading chunks
+    public const int distToLoad = 175;
+
+    //The take turn monobehaviour
+    public HexTakeTurn hexTakeTurn;
+
+
+
+
+    // Use this for initialization
+	void Start () {
+        pieces = new Dictionary<Vector2, HexPiece>();
+        selectedTiles = new Dictionary<Vector2, int>();
+        data = new byte[WorldDiameter/chunkSize,WorldDiameter/chunkSize,chunkSize,chunkSize];
+        chunks = new HexChunk[WorldDiameter / chunkSize, WorldDiameter / chunkSize];
+        freeChunks = new List<HexChunk>();
+        for (int q = -WorldRadius; q <= WorldRadius; q++)
+        {
+            for (int r = -WorldRadius; r <= WorldRadius; r++)
+            {
+                if (inWorld(q, r))
+                {
+                    SetBlock(q, r, (byte)(World.PerlinNoise(q, r, 0, 20f, 10, 1.5f) + World.PerlinNoise(q, r, 0, 10f, 5f, 0.5f) + World.PerlinNoise(q, r, 0, 5f, 2.5f, 0.5f)));
+                }
+            }
+        }
+        hexTakeTurn = GetComponent<HexTakeTurn>();
+        hexTakeTurn.SetUpTeams();
+	}
+
+    public byte GetBlockHeightTop(int q, int r)
+    {
+		if (inWorld(q,r)) {
+			return data[(q+ WorldRadius)/chunkSize,(r+ WorldRadius)/chunkSize,(q+ WorldRadius)%chunkSize,(r+WorldRadius)%chunkSize];
+		} else 
+			return 0;
+    }
+
+    public void SetBlock(int q, int r, byte b)
+    {
+        data[(q + WorldRadius) / chunkSize,(r + WorldRadius) / chunkSize,(q + WorldRadius) % chunkSize,(r + WorldRadius) % chunkSize] = b;    
+    }
+    public bool inWorld(int q, int r){
+		
+        if (Mathf.Abs (q) > WorldRadius || Mathf.Abs (r) > WorldRadius)
+						return false;
+		if (q * r >= 0 && Mathf.Abs (q + r) > WorldRadius)
+						return false;
+         
+		return true;
+	}
+
+    float timer = 0f;
+
+
+    List<Vector2> chunksToAdd = new List<Vector2>();
+
+    void LateUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            timer = 0;
+            chunksToAdd.Clear();
+            for (int chunkQ = -WorldRadius / chunkSize; chunkQ <= WorldRadius / chunkSize; chunkQ++)
+            {
+                for (int chunkR = -WorldRadius / chunkSize; chunkR <= WorldRadius / chunkSize; chunkR++)
+                {
+                    Vector2 hexPosition = new Vector2((chunkQ + 0.5f) *chunkSize, (chunkR + 0.5f )* chunkSize);
+                    Vector2 normalPosition = HexToNormal(hexPosition);
+                    int chunkQIndex = chunkQ + WorldRadius / chunkSize;
+                    int chunkRIndex = chunkR + WorldRadius  / chunkSize;
+                    HexChunk chunk = chunks[chunkQIndex, chunkRIndex];
+                    Vector2 playerPosition = new Vector2(player.transform.position.x, player.transform.position.z);
+                    if (chunk != null && Vector2.Distance(normalPosition, playerPosition) > distToUnload) 
+                    {
+                        chunk.Deallocate();
+                        freeChunks.Add(chunk);
+                        chunks[chunkQIndex, chunkRIndex] = null;
+                    }
+                    else if (chunk == null && Vector2.Distance(normalPosition, playerPosition) < distToLoad)
+                    {
+                        chunksToAdd.Add(new Vector2(chunkQ, chunkR));
+                    }
+                }
+            }
+            foreach (Vector2 hexPosition in chunksToAdd)
+            {
+                //Take a free chunk if we have one availiable
+                if (freeChunks.Count > 0)
+                {
+                    HexChunk chunk = freeChunks[0];
+                    freeChunks.RemoveAt(0);
+                    chunk.ChunkQ = (int) hexPosition.x;
+                    chunk.ChunkR = (int) hexPosition.y;
+                    chunks[chunk.ChunkQ + WorldRadius / chunkSize, chunk.ChunkR + WorldRadius / chunkSize] = chunk;
+                    chunk.update = true;
+                }
+                //We need to instantiate a new object
+                else
+                {
+                    GameObject chunkObject = Instantiate(ChunkPrefab) as GameObject;
+                    HexChunk chunk = chunkObject.GetComponent<HexChunk>();
+                    chunk.ChunkQ = (int) hexPosition.x;
+                    chunk.ChunkR = (int) hexPosition.y;
+                    chunk.hexWorld = this;
+                    chunks[chunk.ChunkQ + WorldRadius / chunkSize, chunk.ChunkR + WorldRadius /chunkSize] = chunk;
+                    chunk.update = true;
+                }
+            }
+        }
+    }
+
+    //Convert Hexagon coordinates to Normal XZ coordinates
+    public Vector2 HexToNormal(Vector2 vector)
+    {
+        Vector2 newVector = Vector2.zero;
+        newVector += vector.y * new Vector2(0, -Root3);
+        newVector += vector.x * new Vector2(1.5f, -Root3 / 2);
+        return newVector;
+    }
+
+
+    public Vector2 NormalToHex(Vector2 vector)
+    {
+        float q = vector.x / 1.5f;
+        return new Vector2(q, (vector.y + Root3 * q) / (-Root3 / 2));
+    }
+   
+}
---
+++ VoxelTutorial/Assets/Scripts/HexChunk.cs
@@ -0,0 +1,365 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ *
+ * Hex chunk is the mesh generator monobehaviour that will
+ * be used for each chunk object in the game
+ */
+public class HexChunk : MonoBehaviour {
+    
+    //The world data
+	public GenerateHexagon hexWorld;
+
+    //The q and r positions of the chunk which is the actual position\
+    //divided by the chunk size
+	public int ChunkQ;
+	public int ChunkR;
+
+    //List of vertices triangles and uvs for mesh generation
+	private List<Vector3> vertices = new List<Vector3>();
+	private List<int> triangles = new List<int>();
+	private List<Vector2> uvs = new List<Vector2>();
+
+    //Vertex offset for keeping track of what vertice was 
+    //added in mesh generation
+	private int vertexOffset = 0;
+
+    //Height of each individual tile in unity blocks
+	private float TILE_HEIGHT = GenerateHexagon.TILE_HEIGHT;
+	private float Root3 = Mathf.Sqrt(3);
+    //public Vector2 mapPosition;
+
+    private bool _update;
+    //Set to true when you want program to update
+    public bool update
+    {
+        get { return _update; }
+        set
+        {
+            if (value == false)
+                Debug.LogError("Can't set update of a hex chunk to false");
+            _update = true;
+        }
+    }
+
+    private List<GameObject> pieces = new List<GameObject>();
+    //Has the information been reset
+    private bool free = true;
+
+    //Update mesh when setting to update
+    void Update()
+    {
+        if (_update)
+        {
+            _update = false;
+            if (!free)
+            {
+                Deallocate();
+            }
+            StartBuilding();
+        }
+    }
+
+    //Deallocate mesh and reset all variables
+    public void Deallocate()
+    {
+        free = true;
+        vertexOffset = 0;
+        vertices.Clear();
+        triangles.Clear();
+        uvs.Clear();
+        GetComponent<MeshFilter>().mesh.Clear();
+        foreach (GameObject obj in pieces)
+        {
+            Destroy(obj);
+        }
+        pieces.Clear();
+    }
+
+
+
+
+
+
+
+	//Build the mesh from the current data in the world at this chunk position
+    private void StartBuilding()
+    {
+        free = false;
+        //Cycle through tile positions in the chunks boundaries
+        for (int q = ChunkQ * GenerateHexagon.chunkSize; q < (ChunkQ + 1) * GenerateHexagon.chunkSize; q++)
+        {
+            for (int r = ChunkR * GenerateHexagon.chunkSize; r < (ChunkR + 1) * GenerateHexagon.chunkSize; r++)
+            {
+                if (hexWorld.inWorld(q, r))
+                {
+                    //Get the x z position of your tiles
+                    Vector2 normalPos = hexWorld.HexToNormal(new Vector2(q, r));
+
+                    Vector3 position = new Vector3(normalPos.x, 0, normalPos.y);
+
+                    //Get the height of the tile
+                    int height = hexWorld.GetBlockHeightTop(q, r);
+
+                    //Add the y position of the tile
+                    position += new Vector3(0, height * TILE_HEIGHT, 0);
+
+                    //Get all the necessary heights of the sides of the tile
+                    //Because if the tile adjacent is lower than this tile then the side height 
+                    //will be taller. if height <= 0 no side will be made
+                    byte bottomLeft = hexWorld.GetBlockHeightTop(-1 + q, 1 + r);
+                    byte bottom = hexWorld.GetBlockHeightTop(q, 1 + r);
+                    byte bottomRight = hexWorld.GetBlockHeightTop(1 + q, r);
+                    byte topLeft = hexWorld.GetBlockHeightTop(q - 1, r);
+                    byte top = hexWorld.GetBlockHeightTop(q, -1 + r);
+                    byte topRight = hexWorld.GetBlockHeightTop(q + 1, r - 1);
+
+                    //Build all the sides
+                    HexSideBottomLeft(position, height - bottomLeft, 0);
+                    HexSideBottomRIght(position, height - bottomRight, 0);
+                    HexSideBottom(position, height - bottom, 0);
+                    HexSideTop(position, height - top, 0);
+                    HexSideTopLeft(position, height - topLeft, 0);
+                    HexSideTopRight(position, height - topRight, 0);
+                    HexTop(position, 1, hexWorld.hexTakeTurn.selectedTiles.ContainsKey(new Vector2(q, r)));
+
+                    //Check the dictionary of pieces. If there is one at this position
+                    //instantiate it to the world.
+                    HexPiece piece;
+                    if (hexWorld.pieces.TryGetValue(new Vector2(q, r), out piece))
+                    {
+                        GameObject obj = Instantiate(hexWorld.piecePrefab) as GameObject;
+                        obj.transform.position = new Vector3(1.5f * q + 0.5f, height / 2.0f + 0.5f, r * (-Root3) + q * (-Root3 / 2) - Root3 / 2);
+                        HexPieceMon hexPiece = obj.GetComponent<HexPieceMon>();
+                        hexPiece.hexPiece = piece;
+                        obj.GetComponent<Renderer>().material.color = piece.team.color;
+                        pieces.Add(obj);
+                    }
+
+                }
+            }
+        }
+
+        //Reset the mesh and set it to the new verices after we calculated the polygons
+        MeshFilter filter = GetComponent<MeshFilter>();
+        Mesh mesh = filter.mesh;
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            filter.mesh = mesh;
+        }
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.Optimize();
+        mesh.RecalculateNormals();
+
+        MeshCollider col = GetComponent<MeshCollider>();
+        col.sharedMesh = null;
+        col.sharedMesh = mesh;
+    }
+	
+	
+
+    //The below methods are all for generating all the triangles for top and sides
+
+	void HexTop(Vector3 topLeft, byte block, bool isSelected){
+		vertices.Add(topLeft+ new Vector3(0, 0, 0));
+		vertices.Add(topLeft + new Vector3(1, 0, 0));
+		vertices.Add(topLeft + new Vector3(1, 0, -Root3));
+		vertices.Add(topLeft + new Vector3(0, 0, -Root3));
+		vertices.Add(topLeft + new Vector3(-0.5f, 0, -0.5f * Root3));
+		vertices.Add(topLeft + new Vector3(1.5f, 0, -0.5f * Root3));
+					
+					
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 2);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 4);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 5);
+		triangles.Add(vertexOffset + 2);
+
+
+        if (!isSelected)
+        {
+            uvs.Add(new Vector2(0.125f, 1));
+            uvs.Add(new Vector2(0.26125f, 1));
+            uvs.Add(new Vector2(0.26125f, 0));
+            uvs.Add(new Vector2(0.125f, 0));
+            uvs.Add(new Vector2(0, 0.5f));
+            uvs.Add(new Vector2(0.5f, 0.5f));
+        }
+        else
+        {
+            uvs.Add(new Vector2(0.6125f, 1));
+            uvs.Add(new Vector2(0.76125f, 1));
+            uvs.Add(new Vector2(0.76125f, 0));
+            uvs.Add(new Vector2(0.6125f, 0));
+            uvs.Add(new Vector2(0.5f, 0.5f));
+            uvs.Add(new Vector2(1, 0.5f));
+        }
+		    
+					
+					
+		vertexOffset += 6;
+	}
+				
+	void HexSideTopLeft(Vector3 topLeft, int height, byte block)
+	{
+        if (height <= 0)
+            return;
+		vertices.Add(topLeft + new Vector3(0, 0, 0));
+		vertices.Add(topLeft + new Vector3(0, -TILE_HEIGHT * height, 0));
+		vertices.Add(topLeft + new Vector3(-0.5f, 0, -0.5f * Root3));
+		vertices.Add(topLeft + new Vector3(-0.5f, -TILE_HEIGHT * height, -0.5f * Root3));
+					
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 2);
+		triangles.Add(vertexOffset + 3);
+		vertexOffset += 4;
+					
+		uvs.Add(new Vector2(0, 0.5f));
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(0.5f, 0.5f));
+		uvs.Add(new Vector2(0.5f, 0));
+	}
+				
+	void HexSideTop(Vector3 topLeft, int height,  byte block)
+	{
+        if (height <= 0)
+            return;
+		vertices.Add(topLeft + new Vector3(0, 0, 0));
+		vertices.Add(topLeft + new Vector3(0, -TILE_HEIGHT * height, 0));
+		vertices.Add(topLeft + new Vector3(1, 0, 0));
+		vertices.Add(topLeft + new Vector3(1, -TILE_HEIGHT * height, 0));
+					
+					
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 2);
+					
+		uvs.Add(new Vector2(0, 0.5f));
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(0.5f, 0.5f));
+		uvs.Add(new Vector2(0.5f, 0));
+					
+		vertexOffset += 4;
+					
+	}
+				
+	void HexSideTopRight(Vector3 topLeft, int height, byte block)
+	{
+        if (height <= 0)
+            return;
+		vertices.Add(topLeft + new Vector3(1, 0, 0));
+		vertices.Add(topLeft + new Vector3(1, -TILE_HEIGHT * height, 0));
+		vertices.Add(topLeft + new Vector3(1.5f, 0, -0.5f * Root3));
+		vertices.Add(topLeft + new Vector3(1.5f, -TILE_HEIGHT * height, -0.5f * Root3));
+					
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 2);
+		triangles.Add(vertexOffset + 0);
+					
+		uvs.Add(new Vector2(0, 0.5f));
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(0.5f, 0.5f));
+		uvs.Add(new Vector2(0.5f, 0));
+					
+		vertexOffset += 4;
+	}
+				
+	void HexSideBottom(Vector3 topLeft, int height, byte block)
+	{
+        if (height <= 0)
+            return;
+		vertices.Add(topLeft + new Vector3(0, 0, -Root3));
+		vertices.Add(topLeft + new Vector3(0, -TILE_HEIGHT * height, -Root3));
+		vertices.Add(topLeft + new Vector3(1, 0, -Root3));
+		vertices.Add(topLeft + new Vector3(1, -TILE_HEIGHT * height, -Root3));
+					
+					
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 2);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 0);
+					
+		uvs.Add(new Vector2(0, 0.5f));
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(0.5f, 0.5f));
+		uvs.Add(new Vector2(0.5f, 0));
+					
+					
+		vertexOffset += 4;
+	}
+				
+				
+	void HexSideBottomLeft(Vector3 topLeft, int height, byte block)
+	{
+        if (height <= 0)
+            return;
+		vertices.Add(topLeft + new Vector3(0, 0, -Root3));
+		vertices.Add(topLeft + new Vector3(0, -TILE_HEIGHT * height, -Root3));
+		vertices.Add(topLeft + new Vector3(-0.5f, 0, -0.5f * Root3));
+		vertices.Add(topLeft + new Vector3(-0.5f, -TILE_HEIGHT * height, -0.5f * Root3));
+					
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 2);
+		triangles.Add(vertexOffset + 0);
+		vertexOffset += 4;
+					
+		uvs.Add(new Vector2(0, 0.5f));
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(0.5f, 0.5f));
+		uvs.Add(new Vector2(0.5f, 0));
+	}
+				
+	void HexSideBottomRIght(Vector3 topLeft, int height, byte block)
+	{
+        if (height <= 0)
+            return;
+		vertices.Add(topLeft + new Vector3(1, 0, -Root3));
+		vertices.Add(topLeft + new Vector3(1, -TILE_HEIGHT * height, -Root3));
+		vertices.Add(topLeft + new Vector3(1.5f, 0, -0.5f * Root3));
+		vertices.Add(topLeft + new Vector3(1.5f, -TILE_HEIGHT * height, -0.5f * Root3));
+					
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 3);
+		triangles.Add(vertexOffset + 1);
+		triangles.Add(vertexOffset + 0);
+		triangles.Add(vertexOffset + 2);
+		triangles.Add(vertexOffset + 3);
+					
+		uvs.Add(new Vector2(0, 0.5f));
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(0.5f, 0.5f));
+		uvs.Add(new Vector2(0.5f, 0));
+					
+		vertexOffset += 4;
+					
+	}
+}
